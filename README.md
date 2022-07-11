26f0390d01ef5415fb6b6674b1f7387e5f219a2c
Alvin218 <1063572720@qq.com>
2022-07-11 17:40:03 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-gantt-basic-cy6k4z
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-gantt-basic-cy6k4z)
---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <DefaultTheme>
+    <Gantt :tasks="tasks" :links="links" :scales="scales" :columns="columns" />
+  </DefaultTheme>
+</template>
+
+<script>
+import { Gantt, DefaultTheme } from '@dhtmlx/trial-vue-gantt';
+import { columns, scales, tasks, links } from './data';
+
+export default {
+  name: 'App',
+  components: {
+    Gantt,
+    DefaultTheme,
+  },
+  data: () => ({
+    columns,
+    scales,
+    tasks,
+    // links,
+  }),
+};
+</script>
+<style scope>
+* {
+  background-color: rgba(10, 47, 68, 0.9);
+}
+</style>
